32627f8c8864f18e042e41974c2d66a0b0d01c6a
shekaruid <shekaruid@gmail.com>
2020-11-03 16:44:16 +0530

    React Context API commit

---
+++ src/pages/react-context-api/UserIndex.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { UserProvider } from './UserContext';
+import Name from './NameComponent';
+import Location from './LocationComponent';
+import UserForm from './UserForm'
+
+// This component updates with data from context
+function UserIndex() {
+    return (
+        <UserProvider>
+            <div className="App">
+                <h2 className="is-size-4">
+                    We'll use the React Context API to pass and receive data in
+                    any component.
+                </h2>
+                <div className="container">
+                    <UserForm />
+
+                    <h2 className="is-size-4">Display User Info Here 👇</h2>
+                    <p>
+                        These two children components will receive data. These could
+                        be nested components.
+                    </p>
+                    <Name />
+                    <Location />
+                </div>
+            </div>
+        </UserProvider>
+    )
+}
+
+export default UserIndex
